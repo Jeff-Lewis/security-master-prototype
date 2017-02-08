f66b09a62f6fbfe61ba5f3a3f399df5fcf701763
Jacob Seward <jacobseward@Jacobs-MBP-2.home>
2017-02-07 22:06:37 -0500

    Resolve Cusip Add Errors
    Had to modify "create" the way I did because of the in-memory API.  Once we have a real server backend, then we can go back to strong-typing the cusip, if needed.

--- src/app/services/cusip.service.ts
+++ src/app/services/cusip.service.ts
@@ -2,18 +2,34 @@ import { Injectable } from '@angular/core';
 
 import { LogHelper } from '../helpers/log.helper';
 import { Cusip, CusipSetupTransition } from '../models/cusip-models';
-import { SetupTransition } from '../models/setup-transition-models';
+import { SetupTransition, SetupTransitionWorkflow } from '../models/setup-transition-models';
 import { HttpApiService } from '../services/http-api.service';
+import { WorkflowService } from '../services/workflow.service';
 
 @Injectable()
 export class CusipService {
   private apiUrl = 'api/cusips';
   
-  constructor(
-    private httpApi: HttpApiService) { }
+  private workflows: SetupTransitionWorkflow[];
 
+  constructor(
+    private httpApi: HttpApiService,
+    private workflowService: WorkflowService) { }
+
+    
   create(cusipName: string): Promise<Cusip> {
-    return this.httpApi.post(this.apiUrl, {name: cusipName, statusId: 1, addedDate: new Date()});
+    return new Promise<Cusip>((resolve) => {
+      this.getWorkflows().then(workflows => {
+        let initialTransition = CusipSetupTransition.create(1000, new Date(), this.workflows[0].current);
+        let cusip = {
+          name: cusipName,
+          addedDate: new Date(),
+          transitions: [initialTransition]
+        };
+        
+        this.httpApi.post(this.apiUrl, cusip).then(response => resolve(response as Cusip));
+      });
+    });        
   }
 
   addTransition(cusip:Cusip, transition:SetupTransition): Promise<Cusip> {
@@ -37,5 +53,19 @@ export class CusipService {
   getCusipByName(name:string): Promise<Cusip> {
     const url = `${this.apiUrl}?name=${name}`;
     return this.httpApi.getSingle<Cusip>(url);
+  }
+
+  private getWorkflows(): Promise<SetupTransitionWorkflow[]> {
+    return new Promise<SetupTransitionWorkflow[]> ((resolve) => {
+        if (this.workflows){
+          resolve(this.workflows);
+          return;
+        }
+
+        this.workflowService.getWorkflows().then(workflows => {
+          this.workflows = workflows;
+          resolve(this.workflows);
+      });
+    });     
   }
 }
